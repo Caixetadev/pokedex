d53b97f01cb97b4fd3f0e0106faf02fbb10268cb
Caixeta <caixetadev@gmail.com>
2021-11-08 22:20:07 -0300

    scroll infinito

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 const pokemonAll = document.querySelector('.containerAll')
 
+let limit = 36
+let seila = 0
+
 const getPost = async (teste) => {
     const url = await fetch(`https://pokeapi.co/api/v2/${teste}`)
     return url.json()
 }
 
 const addDom = async () => {
-    const pokemon = await getPost(`pokemon?limit=36&offset=0`)
+    const pokemon = await getPost(`pokemon?limit=${limit}&offset=${seila}`)
     pokemon.results.map(item => {
         pokeImage(item.url)
     })
@@ -15,11 +18,26 @@ const addDom = async () => {
 const pokeImage = async (image) => {
     const formatado = image.replace('https://pokeapi.co/api/v2/', '')
     const dados = await getPost(formatado)
-
     pokemonAll.innerHTML += `
     <h1>${dados.name}</h1>
-    <img src="${dados.sprites.front_default}">
+    <img src="${dados.sprites.other['official-artwork'].front_default}">
     `
 }
+
+const getNextPosts = () => {
+    setTimeout(() => {
+        seila +=36
+        console.log(seila)
+        addDom()
+    }, 300)
+}
+
+window.addEventListener('scroll', () => {
+    const { clientHeight, scrollHeight, scrollTop } = document.documentElement
+
+    if (scrollTop + clientHeight >= scrollHeight - 10) {
+        getNextPosts()
+    }
+})
 
 addDom()
