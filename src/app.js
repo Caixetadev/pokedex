2ed2c73fd42f2ea8ec8adccea906b417c40ff354
Caixeta <caixetadev@gmail.com>
2021-11-09 20:09:24 -0300

    colocando o segundo type do pokemon

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ const pokeImage = async (image) => {
             <h3>${dados.name}</h3>
             <span>${dados.id}</span>
         </header>
-        <p data-type="${dados.types['0'].type.name}">${dados.types['0'].type.name}</p>
+        <p data-type="${dados.types['0'].type.name}">${dados.types[0].type.name} <br>${dados.types.length === 2 ? dados.types[1].type.name : ''}</p>
         <img class="pokeImage" src="${dados.sprites.other['official-artwork'].front_default}">    
     </div>
     `
@@ -152,8 +152,10 @@ const typePokemon = async (url) => {
             <h3>${dados.name}</h3>
             <span>${dados.id}</span>
         </header>
-        <p data-type="${dados.types['0'].type.name}">${dados.types['0'].type.name}</p>
+        <p data-type="${dados.types['0'].type.name}">${dados.types[0].type.name} <br> ${dados.types.length === 2 ? dados.types[1].type.name : ''}</p>
         <img class="pokeImage" src="${dados.sprites.other['official-artwork'].front_default}">    
     </div>
     `
+    seila()
+    
 }
